1b091cc5757d635cdb5b32a5fa31049224542183
mogo <2083491778@qq.com>
2020-04-26 17:34:28 +0800

    init 2

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
+import BaiduMap from 'vue-baidu-map'
+
 
 Vue.use(Button)
+Vue.use(BaiduMap, {
+    // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
+    ak: 'DCCk7iXtHvPHLMcwY89rPmbs05dRxpwv'
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Hello from '../components/Hello.vue'
+import Hello2 from '../components/Hello2.vue'
+import Hello3 from '../components/Hello3.vue'
+
 
 Vue.use(VueRouter)
 
   const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+      {path:'/hello',component:Hello},
+      {path:'/hello2',component:Hello2},
+      {path:'/hello3',component:Hello3}
+
 ]
 
 const router = new VueRouter({
